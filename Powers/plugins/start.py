ab7b89acf87477a7333107c878dd51fc46e2b128
Kid_of_devil <82517468+tz-ash@users.noreply.github.com>
2024-04-16 21:19:24 +0530

    Update start.py

--- Powers/plugins/start.py
+++ Powers/plugins/start.py
@@ -32,7 +32,7 @@ Your donation might also me get me a new feature or two, which I wasn't able to 
 
 All the fund would be put into my services such as database, storage and hosting!
 
-You can donate by contacting my owner: [Captain Ezio](http://t.me/iamgojoof6eyes)
+You can donate by contacting my owner: [Am Kid](http://t.me/amkidbot)
      """
 
     LOGGER.info(f"{m.from_user.id} fetched donation text in {m.chat.id}")
@@ -107,11 +107,11 @@ async def start(c: Gojo, m: Message):
                 
         try:
             cpt = f"""
-Hey [{m.from_user.first_name}](http://t.me/{m.from_user.username})! I am Gojo ✨.
+Hey [{m.from_user.first_name}](http://t.me/{m.from_user.username})! I am Jaan ✨.
 I'm here to help you manage your group(s)!
 Hit /help to find out more about how to use me in my full potential!
 
-Join my [News Channel](https://t.me/gojo_bots_network) to get information on all the latest updates."""
+Join my [News Channel](https://t.me/art_loop) to get information on all the latest updates."""
 
             await m.reply_photo(
                 photo=str(choice(StartPic)),
@@ -146,11 +146,11 @@ Join my [News Channel](https://t.me/gojo_bots_network) to get information on all
 async def start_back(_, q: CallbackQuery):
     try:
         cpt = f"""
-Hey [{q.from_user.first_name}](http://t.me/{q.from_user.username})! I am Gojo ✨.
+Hey [{q.from_user.first_name}](http://t.me/{q.from_user.username})! I am Jaan ✨.
 I'm here to help you manage your group(s)!
 Hit /help to find out more about how to use me in my full potential!
 
-Join my [News Channel](http://t.me/gojo_bots_network) to get information on all the latest updates."""
+Join my [News Channel](http://t.me/art_loop) to get information on all the latest updates."""
 
         await q.edit_message_caption(
             caption=cpt,
@@ -168,7 +168,7 @@ async def commands_menu(_, q: CallbackQuery):
     keyboard = ikb(ou, True)
     try:
         cpt = f"""
-Hey **[{q.from_user.first_name}](http://t.me/{q.from_user.username})**! I am Gojo✨.
+Hey **[{q.from_user.first_name}](http://t.me/{q.from_user.username})**! I am Jaan✨.
 I'm here to help you manage your group(s)!
 Commands available:
 × /start: Start the bot
@@ -241,7 +241,7 @@ async def help_menu(_, m: Message):
             ou = await gen_cmds_kb(m)
             keyboard = ikb(ou, True)
             msg = f"""
-Hey **[{m.from_user.first_name}](http://t.me/{m.from_user.username})**!I am Gojo✨.
+Hey **[{m.from_user.first_name}](http://t.me/{m.from_user.username})**!I am Jaan✨.
 I'm here to help you manage your group(s)!
 Commands available:
 × /start: Start the bot
